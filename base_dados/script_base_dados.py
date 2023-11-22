239e916b6cb2dc352e4ce0072be498b686fe207a
Bernardo Freitas <a79295@ualg.pt>
2023-11-22 17:08:00 +0000

    Atualização da base de dados, criacao da tabela faturas e insercao da coluna password em clientes

--- base_dados/script_base_dados.py
+++ base_dados/script_base_dados.py
@@ -9,7 +9,8 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS clientes (
                     apelido TEXT,
                     telefone TEXT,
                     data_nascimento DATE,
-                    email TEXT
+                    email TEXT,
+                    password TEXT,
                 )''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS veiculos (
@@ -21,6 +22,21 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS veiculos (
                     matricula TEXT,
                     nif INTEGER,
                     FOREIGN KEY (nif) REFERENCES clientes(nif)
+                )''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS faturas (
+                    numero_fatura INTEGER PRIMARY KEY,
+                    nif_cliente INTEGER,
+                    nome_cliente TEXT,
+                    apelido_cliente TEXT,
+                    telefone_cliente TEXT,
+                    matricula TEXT,
+                    marca TEXT,
+                    modelo TEXT,
+                    descricao_servico TEXT,
+                    valor DECIMAL,
+                    FOREIGN KEY (nif_cliente) REFERENCES clientes(nif),
+                    FOREIGN KEY (matricula, marca, modelo) REFERENCES veiculos(matricula, marca, modelo)
                 )''')
 
 conexao.commit()
