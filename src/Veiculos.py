0a40ca33e0ceeb087c8a51605cfc3da424fcb3d6
Tomas Anastacio <a79301@ualg.pt>
2023-11-22 17:32:10 +0000

    implementacao da funcao para listar os veiculos pertencentes a um cliente

--- src/Veiculos.py
+++ src/Veiculos.py
@@ -28,3 +28,30 @@ def listar_todos_veiculos():
     print(tabulate(dados_tabela, headers=cabecalho, tablefmt="pipe"))
 
     conexao.close()
+
+
+def listar_carros_cliente(nif_cliente):
+    conexao = sqlite3.connect('../database.db')
+    cursor = conexao.cursor()
+
+    query_dados_cliente = '''SELECT c.nome, v.id_veiculo, v.marca, v.modelo, v.matricula, v.ano, v.cor
+                                 FROM clientes c
+                                 INNER JOIN veiculos v ON c.nif = v.nif
+                                 WHERE c.nif = ?'''
+
+    cursor.execute(query_dados_cliente, (nif_cliente,))
+    dados_cliente_veiculos = cursor.fetchall()
+
+    if dados_cliente_veiculos:
+        cabecalho = ["Id Veiculo", "Marca", "Modelo", "Matricula", "Ano", "Cor"]
+
+        nome_cliente = dados_cliente_veiculos[0][0]
+        veiculos_cliente = [veiculo[1:] for veiculo in dados_cliente_veiculos]
+
+        print(f"Carros do cliente {nome_cliente} com NIF {nif_cliente}:\n")
+        print(tabulate(veiculos_cliente, headers=cabecalho, tablefmt="pretty"))
+
+    else:
+        print(f"O cliente com NIF {nif_cliente} não possui carros.")
+
+    conexao.close()
